7b7ae46d5b03fbf63c25f0003d1cadf04e5ab9d8
Keks <keks@htmlacademy.ru>
2022-10-10 10:31:08 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -1,12 +1,24 @@
-function getRandomIntInclusive(min, max) {//https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+//https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+getRandomIntInclusive(1,5);
+function getRandomIntInclusive(min, max) {
+
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  if (min === max || min > max) {
+    return NaN;}
+
+  else if(min >= 0 && max >= 0){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  else {
+    return NaN;
+  }
 }
-getRandomIntInclusive(1,5);
+getRandomIntInclusive(0,-2);
 
 function checkMaxLengthOfString(checkString, maxLength) {
-  if(checkString.length < maxLength){
+  if(checkString.length <= maxLength){
     return true;
   }
 
