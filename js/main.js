f2a0429f6788cf3d26d339696b0b43a2ee6572fa
Qwerty <dalavegomgdn@gmail.com>
2022-10-10 17:37:30 +1000

    Remove an extra line

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 //https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-getRandomIntInclusive(1,5);
 function getRandomIntInclusive(min, max) {
 
   min = Math.ceil(min);
