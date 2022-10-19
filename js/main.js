dd98abcb0cd645be2f10a5803ae8861ee5a655d6
Qwerty <dalavegomgdn@gmail.com>
2022-10-20 00:14:04 +1000

    Fixes in main.js: rework func 'createComments'

--- js/main.js
+++ js/main.js
@@ -1,45 +1,27 @@
-// Функция взята из интернета и доработана
-// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
-
 function getRandomPositiveInteger (a, b) {
-  // Если переданы отрицительные числа, возвращаем NaN
+
   if (a < 0 || b < 0) {
     return NaN;
   }
 
-  // Чтобы не заставлять пользователя нашей функции помнить порядок аргументов,
-  // реализуем поддержку передачи минимального и максимального значения в любом порядке,
-  // а какое из них большее и меньшее вычислим с помощью Math.min и Math.max.
-
-  // После нам нужно убедиться, что пользователь не передал дробные значения,
-  // для этого на всякий пожарный случай нижнюю границу диапазона
-  // мы округляем к ближайшему большему целому с помощью Math.ceil,
-  // а верхнюю границу - к ближайшему меньшему целому с помощью Math.floor
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
-
-  // Дальше используем Math.random() для получения случайного дробного числа в диапазоне [0, 1),
-  // которое домножаем на разницу между переданными числами плюс единица - это будет наша случайная дельта.
-  // После нужно сложить дельту с минимальным значением, чтобы получить итоговое случайное число.
   const result = Math.random() * (upper - lower + 1) + lower;
-  // "Плюс единица", чтобы включить верхнюю границу диапазона в случайные числа
 
-  // И в конце с помощью метода Math.floor мы округляем полученный результат,
-  // потому что Math.random() генерирует только дробные числа и ноль.
   return Math.floor(result);
 }
 
-function checkStringLength (string, length) {
-  return string.length <= length;
-}
-checkStringLength('privet', 6);
+const AVATARS_COUNT = {
+  MIN: 1,
+  MAX: 6
+};
 
-const AVATARS_COUNT = 6;
-const SIMILAR_OBJECTS_COUNT = 25;
 const LIKES_COUNT = {
   MIN: 15,
   MAX: 200
 };
+
+const SIMILAR_OBJECTS_COUNT = 25;
 
 const NAMES = [
   'Иван',
@@ -70,30 +52,31 @@ const DESCRTIPTIONS = [
 
 const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
 
-//Took from 4.18. Лайв «Ретроспектива. Структуры данных и встроенные API»
 const createMessage = () => Array.from({length: getRandomPositiveInteger(1,2)}, () =>
   getRandomArrayElement(COMMENTS))
   .join(' ');
 
-const createComment = () => ({
-  id: `${String(Date.now()) + Math.floor(Math.random() * 10000)}`,
-  avatar: `img/avatar-${getRandomPositiveInteger(1,AVATARS_COUNT)}.svg`,
+const createComments = () => ({
+  id: `${Date.now() + Math.floor(Math.random() * 10000)}`,
+  avatar: `img/avatar-${getRandomPositiveInteger(AVATARS_COUNT.MIN,AVATARS_COUNT.MAX)}.svg`,
   message: createMessage(),
   name: getRandomArrayElement(NAMES)
 });
 
-const createUserDescription = (id, url) => ({
-  id: id,
-  url: `photos/${url}.jpg`,
+const createUserDescription = (index) => ({
+  id: index,
+  url: `photos/${index}.jpg`,
   description: getRandomArrayElement(DESCRTIPTIONS),
   likes: getRandomPositiveInteger(LIKES_COUNT.MIN, LIKES_COUNT.MAX),
-  comments: createComment()
+  comments: Array.from({
+    length: getRandomPositiveInteger(1,5)}, () =>
+    createComments())
 });
 
-
-const getUserDescription = [];
-for (let i = 1; i <= SIMILAR_OBJECTS_COUNT; i++) {
-  getUserDescription.push(createUserDescription(i,i));
-  createUserDescription.id = i;
-  createUserDescription.url = i;
-}
+const getUserDescription = () => {
+  const userDescription = [];
+  for (let i = 1; i <= SIMILAR_OBJECTS_COUNT; i++) {
+    userDescription.push(createUserDescription(i));
+  }
+  return userDescription;
+};
