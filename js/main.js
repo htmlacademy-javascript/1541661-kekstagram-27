bdd9ba70005abae17614510564ff0f4ac74e0f5b
Qwerty <dalavegomgdn@gmail.com>
2022-11-16 12:03:27 +1000

    Added edit-form.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import {getUserPosts} from './data.js';
 import {renderUserPhotos} from './create-pictures.js';
+import {renderUploadForm} from './edit-form.js';
 
 getUserPosts();
 renderUserPhotos();
+renderUploadForm();
